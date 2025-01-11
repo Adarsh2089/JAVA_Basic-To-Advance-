33ca973e908ecbb816b89e10006f04381fb3027c
Adarsh Kumar <adarsh.mca23@gmail.com>
2025-01-11 13:38:40 +0000

    variables

---
+++ Basics/DataTypes.java
@@ -0,0 +1,34 @@
+package Basics;
+
+public class DataTypes{
+    public static void main (String a[]){
+
+
+
+        short num=128;
+        System.out.println(num);
+    }
+}
+
+/*
+                                         DataTypes
+                                             |
+                        --------------------------------------------  
+                        |                                          |
+               Primitive Datatypes                      Non-Primitive DataTypes
+                        |                                                |
+            --------------------------------------                  -------------------------
+            |            |             |         |
+         Integer       Float       character   Boolean       
+          |             |            |          |
+          |- Byte       |-double     |- char    |- boolean
+          |- short      |-float
+          |- int         
+          |- long
+
+
+
+
+
+
+ */
---
+++ Basics/variables.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class variables {
+    public static void main (String a[]){
+        int num=10; // Local Variable
+        System.out.println(num);
+    }
+
+    
+}
+
+ /*
+         int           num = 10;
+          |             |
+          |             |
+   variable type   variable name  
+   
+
+   ** Types of Variables--
+    <1>   Local Variables -
+                  A variable defined within a block or method or constructor
+    <2>   Instance Variables -
+                                
+ */ 
